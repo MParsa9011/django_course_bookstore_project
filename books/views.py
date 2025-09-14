fe47d7e646720bdd6ef6f3caa72a35fe4f75497e
MParsa9011 <mpsalehi90@gmail.com>
2025-09-14 13:59:49 +0330

    edit BookCreateView and BookUpdateView

--- books/views.py
+++ books/views.py
@@ -19,12 +19,12 @@ class BookDetailView(generic.DetailView):
 
 class BookCreateView(generic.CreateView):
     model = Book
-    fields = ['title', 'author', 'translator', 'publisher', 'content', 'price', 'cover']
+    fields = ['title', 'author', 'translator', 'publisher', 'content', 'pages', 'price', 'cover', 'status']
     template_name = 'books/book_create.html'
 
 class BookUpdateView(generic.UpdateView):
     model = Book
-    fields = ['title', 'author', 'translator', 'publisher', 'content', 'price', 'cover']
+    fields = ['title', 'author', 'translator', 'publisher', 'content', 'pages', 'price', 'cover', 'status']
     template_name = 'books/book_update.html'
 
 class BookDeleteView(generic.DeleteView):
